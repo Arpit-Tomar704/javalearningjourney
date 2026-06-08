875c7571ca32db8bc6fcec036c42f1efb8b462ee
Arpit Tomar <arpittomar704@gmail.com>
2026-06-08 23:21:39 +0530

    learning

--- src/Challenges/cha_25RightshiftOperator.java
+++ src/Challenges/cha_25RightshiftOperator.java
@@ -2,13 +2,13 @@ package Challenges;
 
 import java.util.Scanner;
 
-public class cha25_Leftshift {
+public class cha_25RightshiftOperator {
     static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("showcasing leftshiftoperator\n");
+        System.out.println("showcasing Rightshiftoperator\n");
         System.out.print("please enter your number : ");
         int num = input.nextInt();
-        int result = num <<1 ;
+        int result = num >>1 ;
         System.out.println("your result is : "+result);
     }
 }
