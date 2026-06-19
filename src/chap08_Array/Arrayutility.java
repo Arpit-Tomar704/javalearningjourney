44cdda1e42f8f52a768b2f5357584b1942931b79
Arpit Tomar <arpittomar704@gmail.com>
2026-06-19 12:32:15 +0530

    Update Arrayutility.java

--- src/chap08_Array/Arrayutility.java
+++ src/chap08_Array/Arrayutility.java
@@ -19,4 +19,14 @@ public class Arrayutility {
 
     }
 
+
+    public static void displayArray(int[]numArray){
+        int i =0;
+        while (i<numArray.length){
+            System.out.println(numArray[i] + "");
+            i++;
+        }
+        System.out.println();
+    }
+
 }
