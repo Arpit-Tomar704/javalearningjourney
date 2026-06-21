615d87df69015c0db1306d02949a28f0787608e2
Arpit Tomar <arpittomar704@gmail.com>
2026-06-21 22:21:15 +0530

    commit

---
+++ src/Challenges/cha41_Search2DArray.java
@@ -0,0 +1,40 @@
+package Challenges;
+
+
+import chap08_Array.Arrayutility;
+
+import java.util.Scanner;
+
+public class cha41_Search2DArray {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("welcome to 2D search \n");
+        int[][] numArr = Arrayutility.input2DArray();
+        System.out.print("now enter the number you want to search : ");
+        int num = input.nextInt();
+        boolean isfound = search(numArr,num);
+        if (isfound){
+            System.out.println("your number was found");
+        }else {
+            System.out.println("your number was not found ");
+        }
+
+    }
+    public static  boolean search(int[][]numArr,int num){
+        int i =0;
+        while (i<numArr.length){
+            int j = 0;
+            while (j<numArr[i].length){
+                if (numArr[i][j]==num){
+                    return true;
+                }
+                j++;
+            }
+            i++;
+        }
+        return false;
+    }
+
+
+
+}
--- src/chap08_Array/Arrayutility.java
+++ src/chap08_Array/Arrayutility.java
@@ -2,6 +2,8 @@ package chap08_Array;
 
 import java.util.Scanner;
 
+
+
 public class Arrayutility {
     public static int[] inputArray(){
         Scanner input = new Scanner(System.in);
@@ -12,6 +14,33 @@ public class Arrayutility {
         while (i<size){
             System.out.print("Please enter  element no " +(i+1)+": ");
             nums[i] = input.nextInt();
+            i++;
+        }
+        return nums;
+
+
+    }
+
+
+    public static int[][] input2DArray(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please Enter The Number of Rows : ");
+        int Rows = input.nextInt();
+        System.out.print("Please Enter The Number of Columns : ");
+        int Columns = input.nextInt();
+        int nums[][] = new int[Rows][Columns];
+        int i =0;
+        while (i<Rows){
+            int j =0;
+            while (j<Columns){
+                System.out.print("Please enter element row  " +(i+1)+
+                                ",column : " +(j+1)+ ":");
+                nums[i] [j] = input.nextInt();
+                j++;
+
+
+            }
+
             i++;
         }
         return nums;
