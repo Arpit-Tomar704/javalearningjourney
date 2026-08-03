77353f215d3423cccd7dd5a44b6a14a2ec970648
Arpit Tomar <arpittomar704@gmail.com>
2026-08-03 23:57:40 +0530

    Update 46 course.java

--- src/Challenges/46 course.java
+++ src/Challenges/46 course.java
@@ -6,8 +6,27 @@ package Challenges;
   int  enrollments;
 
 
+  String[] enrolledStudents ;
+
+  course(String courseName){
+   this.courseName=courseName;
+   this.enrollments =0;
+   this.enrolledStudents = new String[maxCapacity];
+  }
+  
   static void setMaxCapacity(int maxCapacity){
    course.maxCapacity=maxCapacity;
   }
+  void enrollStudent(String studentName){
+   enrolledStudents[enrollments]= studentName;
+   enrollments++;
+  }
+
+  void unenrollStuden(String studentName){
+   System.out.println("student removed");
+   enrollments--;
+
+  }
+
 
 }
