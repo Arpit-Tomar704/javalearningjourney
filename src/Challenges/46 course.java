668cc15935ab14179fb88eb6bc0d066064af0a8e
Arpit Tomar <arpittomar704@gmail.com>
2026-08-03 23:48:11 +0530

    Create 46 course.java

---
+++ src/Challenges/46 course.java
@@ -0,0 +1,13 @@
+package Challenges;
+
+ class course {
+  static  int maxCapacity = 100;
+  String courseName;
+  int  enrollments;
+
+
+  static void setMaxCapacity(int maxCapacity){
+   course.maxCapacity=maxCapacity;
+  }
+
+}
