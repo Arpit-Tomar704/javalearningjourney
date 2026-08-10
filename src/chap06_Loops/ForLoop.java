37999d196b04980f7f2cbb362d5891e855601907
Arpit Tomar <arpittomar704@gmail.com>
2026-08-10 12:36:36 +0530

    Create ForLoop.java

---
+++ src/chap06_Loops/ForLoop.java
@@ -0,0 +1,17 @@
+package chap06_Loops;
+
+import java.util.Scanner;
+
+public class ForLoop {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//        int i=1;
+//        while (i<=100){
+//            System.out.println(i+ " ");
+//            i++;
+//        }
+        for (int i=1;i<=100;i++){
+            System.out.println(i+ " ");
+        }
+    }
+}
