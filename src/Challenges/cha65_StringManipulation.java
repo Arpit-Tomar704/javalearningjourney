2f90d752b603faa664e664d2e881367a46f1e635
Arpit Tomar <arpittomar704@gmail.com>
2026-08-14 17:10:33 +0530

    Create cha65_StringManipulation.java

---
+++ src/Challenges/cha65_StringManipulation.java
@@ -0,0 +1,13 @@
+package Challenges;
+
+
+
+class StringManipulation {
+     static void main(String[] args) {
+         String  firstName = "Arpit";
+         String LastName = "Tomar";
+         String FullName = firstName.concat(" ").concat(LastName);
+          System.out.printf(FullName.toUpperCase());
+
+     }
+}
