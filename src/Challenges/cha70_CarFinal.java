472b0edcd47a6ed2e568217f56efd24f0e84db9f
Arpit Tomar <arpittomar704@gmail.com>
2026-08-16 13:14:29 +0530

    Create cha70_CarFinal.java

---
+++ src/Challenges/cha70_CarFinal.java
@@ -0,0 +1,15 @@
+package Challenges;
+
+ class CarFinal {
+     final int Noofwheels;
+     final String model;
+     final String engineInLitres;
+
+
+     public CarFinal(int noofwheels, String model, String engineInLitres) {
+         Noofwheels = noofwheels;
+         this.model = model;
+         this.engineInLitres = engineInLitres;
+
+     }
+ }
