2590d6af07f92caa7fd7a26b3f1db6ef225ffa76
Arpit Tomar <arpittomar704@gmail.com>
2026-08-16 22:25:32 +0530

    Create DefalutTest.java

---
+++ src/chap15_EncapsulationandInheritance/DefalutTest.java
@@ -0,0 +1,11 @@
+package chap15_EncapsulationandInheritance;
+
+import in.kgcoding.Car;
+
+public class DefalutTest {
+    static void main(String[] args) {
+        Car car = new Car("yellow", "bmw", 5, 3000);
+//        car.costofpurchse =8;
+
+    }
+    }
