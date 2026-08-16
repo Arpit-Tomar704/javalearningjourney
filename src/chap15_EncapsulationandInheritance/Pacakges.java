31c57c977fc502c1a35c15d8bc71d7b6c18aebbe
Arpit Tomar <arpittomar704@gmail.com>
2026-08-16 22:25:38 +0530

    Create Pacakges.java

---
+++ src/chap15_EncapsulationandInheritance/Pacakges.java
@@ -0,0 +1,9 @@
+package chap15_EncapsulationandInheritance;
+
+import java.util.Scanner;
+
+public class Pacakges {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+}
