718c22c9a4422ed30d80eee472f0b654f4440fc7
Arpit Tomar <arpittomar704@gmail.com>
2026-08-18 00:00:26 +0530

    Create Calculator.java

---
+++ src/Challenges77/Packages/Packages/utils/Calculator.java
@@ -0,0 +1,17 @@
+package Challenges77.Packages.Packages.utils;
+
+import Challenges77.Packages.Packages.geometry.Circle;
+import Challenges77.Packages.Packages.geometry.Rectangle;
+
+public class Calculator {
+    static void main(String[] args) {
+        Circle cir = new Circle(5.5);
+        Rectangle rect = new Rectangle(10,5);
+
+        double cirArea = Math.PI*Math.pow(cir.radius,2);
+        double rectArea = rect.length *rect.breadth;
+
+        System.out.printf("Area of the circle is:%f,Area of the rectangle is:%f" ,cirArea,rectArea);
+
+    }
+}
