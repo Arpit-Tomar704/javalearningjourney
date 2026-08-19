684b780f94febbd92e3268020bbb5d6cb11480f6
Arpit Tomar <arpittomar704@gmail.com>
2026-08-19 22:01:03 +0530

    Create MotorCycle.java

---
+++ src/in/kgcoding/inheritance/MotorCycle.java
@@ -0,0 +1,9 @@
+package in.kgcoding.inheritance;
+
+public class MotorCycle  extends  TwoWheeler{
+    public double petrolCapacity;
+
+    public void Start(){
+        System.out.println("Starting");
+    }
+}
