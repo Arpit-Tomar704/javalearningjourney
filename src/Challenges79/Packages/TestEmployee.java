93e9c56abfedb22009722ada894c4f45a86c1dcd
Arpit Tomar <arpittomar704@gmail.com>
2026-08-19 22:00:46 +0530

    Create TestEmployee.java

---
+++ src/Challenges79/Packages/TestEmployee.java
@@ -0,0 +1,10 @@
+package Challenges79.Packages;
+
+public class TestEmployee {
+    static void main(String[] args) {
+        Employee emp = new Employee("Arpit",19,80000);
+        System.out.println(emp.getEmployeeDetails());
+        emp.setName("Ansh");
+        System.out.println(emp.getEmployeeDetails());
+    }
+}
