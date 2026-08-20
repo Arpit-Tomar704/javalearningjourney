7ba107579f79338ad6b69990819aa8dd20b28ff7
Arpit Tomar <arpittomar704@gmail.com>
2026-08-20 08:29:37 +0530

    Create Customer.java

---
+++ src/Challenges72/Packages/Customer.java
@@ -0,0 +1,13 @@
+package Challenges72.Packages;
+
+public class Customer {
+
+    public  static void  main(String[] args){
+        BankAccount account = new BankAccount("001","Arpit Tomar");
+        account.depositMoney(300);
+        System.out.println(account.withDrawMoney(100));
+        account.depositMoney(-50);
+        account.withDrawMoney(0);
+    }
+
+}
