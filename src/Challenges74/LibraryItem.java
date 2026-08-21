92a325265bbf619c7488ad28fbd1384adedaf7d8
Arpit Tomar <arpittomar704@gmail.com>
2026-08-21 21:28:02 +0530

    Create LibraryItem.java

---
+++ src/Challenges74/LibraryItem.java
@@ -0,0 +1,14 @@
+package Challenges74;
+
+public class LibraryItem {
+    private String itemId;
+    private String title;
+    private String author;
+     public void checkout(){
+         System.out.println("checkout");
+     }
+
+     public void returnitem(){
+         System.out.println("retrun the item");
+     }
+}
