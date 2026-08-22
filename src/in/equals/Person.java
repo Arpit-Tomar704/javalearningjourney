932da1ec8cec95b8ae9c5e899ceaee4ea6f3514d
Arpit Tomar <arpittomar704@gmail.com>
2026-08-22 16:45:59 +0530

    Create Person.java

---
+++ src/in/equals/Person.java
@@ -0,0 +1,66 @@
+package in.equals;
+
+import java.util.Objects;
+import java.util.StringJoiner;
+
+public class Person {
+    private  String name;
+    private  int age;
+    private String id;
+
+    public Person(String name, int age, String id) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof  Person)){
+            return false;
+
+        }
+        Person per = (Person) obj;
+        return per.name.equals(name)&&
+                per.age == age &&
+                per.id.equals(id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, id);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Person.class.getSimpleName() + "[", "]")
+                .add("name='" + name + "'")
+                .add("age=" + age)
+                .add("id='" + id + "'")
+                .toString();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
