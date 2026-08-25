0262d533db4813ac579f7949fc555a3fa099d254
Arpit Tomar <arpittomar704@gmail.com>
2026-08-25 15:57:30 +0530

    Create TestAbstraction.java

---
+++ src/chap16_AbstractionandPolymorphism/TestAbstraction.java
@@ -0,0 +1,12 @@
+package chap16_AbstractionandPolymorphism;
+
+public class TestAbstraction {
+
+    static void main(String[] args) {
+
+      // Vehicle veh = new Vehicle(2);
+        Car car = new Car();
+        car.commute();
+
+    }
+}
