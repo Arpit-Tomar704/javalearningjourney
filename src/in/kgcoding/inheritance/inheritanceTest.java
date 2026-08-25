707879a64b1da3e0531fc43da83c83aa1de5065e
Arpit Tomar <arpittomar704@gmail.com>
2026-08-25 15:57:44 +0530

    Update inheritanceTest.java

--- src/in/kgcoding/inheritance/inheritanceTest.java
+++ src/in/kgcoding/inheritance/inheritanceTest.java
@@ -5,6 +5,9 @@ public class inheritanceTest {
     static void main(String[] args) {
         Vehicle vehicle = new Vehicle();
         vehicle.commute();
+        System.out.println(vehicle.toString());
+        System.out.println(vehicle.hashCode());
+        System.out.println(vehicle.getClass());
 
 
         TwoWheeler two = new TwoWheeler();
