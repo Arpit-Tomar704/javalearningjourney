faf39d4e4a8bd919ed550126d2aeb5d540651fff
Arpit Tomar <arpittomar704@gmail.com>
2026-08-26 18:56:32 +0530

    Create Car.java

---
+++ src/Challenges79/Car.java
@@ -0,0 +1,10 @@
+package Challenges79;
+
+public class Car extends  Vehicle {
+
+    @Override
+    public void service() {
+        super.service();
+        System.out.println("car is getting serviced ");
+    }
+}
