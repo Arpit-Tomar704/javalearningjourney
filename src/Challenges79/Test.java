8bb2566c56e8187cae0e000e164904dde60d6a2d
Arpit Tomar <arpittomar704@gmail.com>
2026-08-26 18:56:38 +0530

    Create Test.java

---
+++ src/Challenges79/Test.java
@@ -0,0 +1,10 @@
+package Challenges79;
+
+public class Test {
+    static void main(String[] args) {
+        Car car =new Car();
+        Vehicle veh = new Vehicle();
+        car.service();
+        veh.service();
+    }
+}
