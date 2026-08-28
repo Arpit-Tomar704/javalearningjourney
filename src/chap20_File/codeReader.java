90a89cb8396c99f68587c78aed644ea508ccae83
Arpit Tomar <arpittomar704@gmail.com>
2026-08-28 11:35:56 +0530

    Create codeReader.java

---
+++ src/chap20_File/codeReader.java
@@ -0,0 +1,23 @@
+package chap20_File;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class codeReader {
+    static void main(String[] args) {
+        String fileName = "java-course.txt";
+
+        try(FileReader reader = new FileReader(fileName)){
+            int read = 0;
+            do {
+                read = reader.read();
+                System.out.print((char) read);
+            }while(read != -1);
+
+        }catch (IOException e){
+            System.out.printf("Exception occured: %s" ,e.getMessage());
+
+        }
+
+    }
+}
