e81befaaa5919a3a2387646b225b864822f73108
Arpit Tomar <arpittomar704@gmail.com>
2026-08-29 18:51:24 +0530

    Create EnumTest.java

---
+++ src/challenges89/EnumTest.java
@@ -0,0 +1,10 @@
+package challenges89;
+
+public class EnumTest {
+    static void main(String[] args) {
+        System.out.println("printing all the days of the week : ");
+        for (Day day : Day.values()) {
+            System.out.printf("%s : %s\n",day,day.getType());
+        }
+    }
+}
