f6daeb2dc26da27021a8d6d590edeb5a347a2953
Arpit Tomar <arpittomar704@gmail.com>
2026-08-29 18:53:00 +0530

    Create PrintTask.java

---
+++ src/chap26_MultiThreading/runnable/PrintTask.java
@@ -0,0 +1,24 @@
+package chap26_MultiThreading.runnable;
+
+public class PrintTask implements  Runnable {
+    @Override
+    public void run() {
+
+        //print Task
+        for (int i = 1; i <=1000 ; i++) {
+            System.out.printf("%d%c ",i,targetChar);
+
+        }
+        System.out.printf("\n  %s %c  task complete",
+                Thread.currentThread().getName(),
+                targetChar);
+
+
+    }
+
+    private final char targetChar;
+
+    public PrintTask(char targetChar) {
+        this.targetChar = targetChar;
+    }
+}
