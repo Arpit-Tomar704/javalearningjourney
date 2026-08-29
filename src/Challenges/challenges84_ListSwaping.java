b679bbe39186fcac2124ea7d86f6746e84a8a0db
Arpit Tomar <arpittomar704@gmail.com>
2026-08-29 18:50:28 +0530

    Create challenges84_ListSwaping.java

---
+++ src/Challenges/challenges84_ListSwaping.java
@@ -0,0 +1,25 @@
+package Challenges;
+
+import java.util.Arrays;
+import java.util.List;
+
+class ListSwaping {
+    static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6);
+        System.out.println(list);
+        swap(list,2,4);
+        System.out.println(list);
+
+
+    }
+
+    public static void swap(List<Integer> list,int x,int y){
+        int swap = list.get(x);
+        list.set(x,list.get(y));
+        list.set(y,swap);
+
+    }
+
+
+}
+
