7cf7afdcafdcf1b85a11641cf2fbd2d9297b9a0d
Arpit Tomar <arpittomar704@gmail.com>
2026-08-30 17:16:37 +0530

    Create HelloThread.java

---
+++ src/Challenges91/HelloThread.java
@@ -0,0 +1,21 @@
+package Challenges91;
+public class HelloThread extends Thread {
+
+        private final int threadNumber;
+
+        public HelloThread(int threadNumber) {
+            this.threadNumber = threadNumber;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                System.out.printf("  %d Hello from  Thread - %d\n ",i+1
+                        , threadNumber);
+            }
+        }
+    }
+
+
+
+
